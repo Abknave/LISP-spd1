e834a3181d6de3afacd0225f6ce8c1e441139ff3
root <abhinav.coder@gmail.com>
2015-12-01 17:37:40 +0000

    String lesson

---
+++ Lesson_strings.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Lesson_strings) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+"apple"
+(string-append "Ada" " " "LoveLace")
+
+"123"
+(string-append "1" "123")
+(string-length "123")
+(substring "asdfgjk1" 2 (string-length "asdfgjk1") )
+
+(substring "asdfgjk1" 2)
