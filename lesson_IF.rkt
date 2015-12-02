28081e56bb3db58eb527cb6edc2fb1c2a6dbd34d
root <abhinav.coder@gmail.com>
2015-12-02 13:11:56 +0000

    Lesson IF

---
+++ lesson_IF.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname lesson_IF) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(require 2htdp/image)
+
+(if (< 5 2) "tall" "wide")
+;    boolean  if     else
+;             true 
